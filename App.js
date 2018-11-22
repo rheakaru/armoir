ecb56ca3b6c70d07b66babc501bd3b77c8773ec8
vlahakis <ciscovlahakis@gmail.com>
2018-11-22 11:41:57 -0800

    Added Firebase

--- App.js
+++ App.js
@@ -21,6 +21,7 @@ import Search from './src/search';
 import Showing from './src/showing';
 import Results from './src/results';
 import Item from './src/item';
+import firebase from 'react-native-firebase';
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -35,74 +36,76 @@ export default class App extends Component<Props> {
   }
 
   render() {
-    return (
+      const items_ref = firebase.database().ref("items");
+      console.warn(items_ref);
+
+      return (
       <SafeAreaView style={{ flex: 1, width: '100%' }}>
         <Header />
+        <View
+          style={{
+            flex: 2,
+            flexDirection: 'column',
+            backgroundColor: 'rgb(206,189,173)',
+          }}
+        >
+          <TextInput
+            style={{
+              flex: 1,
+              borderRadius: 10,
+              margin: 20,
+              backgroundColor: 'white',
+            }}
+            onChangeText={text => this.setState({ inputText: text })}
+            value={this.state.inputText}
+          />
           <View
-              style={{
-                  flex: 2,
-                  flexDirection: 'column',
-                  backgroundColor: 'rgb(206,189,173)',
-              }}
+            style={{
+              flex: 1,
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
           >
-              <TextInput
-                  style={{
-                      flex: 1,
-                      borderRadius: 10,
-                      margin: 20,
-                      backgroundColor: 'white'
-                  }}
-                  onChangeText={text => this.setState({ inputText: text })}
-                  value={this.state.inputText}
-              />
-              <View
-                  style={{
-                      flex: 1,
-                      flexDirection: 'row',
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                  }}
-              >
-                  <View style={{ flex: 1 }}>
+            <View style={{ flex: 1 }}>
+              {/*/!*<Picker*!/*/}
+              {/*selectedValue={this.state.selectedCategory}*/}
+              {/*onValueChange={itemValue =>*/}
+              {/*this.setState({ selectedCategory: itemValue })*/}
+              {/*}*/}
 
-                      {/*/!*<Picker*!/*/}
-                      {/*selectedValue={this.state.selectedCategory}*/}
-                      {/*onValueChange={itemValue =>*/}
-                      {/*this.setState({ selectedCategory: itemValue })*/}
-                      {/*}*/}
-
-                      {/*>*/}
-                      {/*<Picker.Item label="All" value="all" />*/}
-                      {/*<Picker.Item label="Shirts" value="shirts" />*/}
-                      {/*/!*</Picker>*!/*/}
-                      <Text>category</Text>
-                  </View>
-                  <View style={{ flex: 1 }}>
-                      {/*<Picker*/}
-                      {/*selectedValue={this.state.selectedFilter}*/}
-                      {/*onValueChange={itemValue =>*/}
-                      {/*this.setState({ selectedFilter: itemValue })*/}
-                      {/*}*/}
-                      {/*style={{ flex: 1 }}*/}
-                      {/*>*/}
-                      {/*<Picker.Item label="filter1" value="filter1" />*/}
-                      {/*<Picker.Item label="filter2" value="filter2" />*/}
-                      {/*</Picker>*/}
-                      <Text>picker1</Text>
-                      {/*<Picker*/}
-                      {/*selectedValue={this.state.selectedSortBy}*/}
-                      {/*onValueChange={itemValue =>*/}
-                      {/*this.setState({ selectedSortBy: itemValue })*/}
-                      {/*}*/}
-                      {/*style={{ flex: 1 }}*/}
-                      {/*>*/}
-                      {/*<Picker.Item label="sortby1" value="sortby1" />*/}
-                      {/*<Picker.Item label="sortby2" value="sortby2" />*/}
-                      {/*</Picker>*/}
-                      <Text>picker2</Text>
-                  </View>
-              </View>
+              {/*>*/}
+              {/*<Picker.Item label="All" value="all" />*/}
+              {/*<Picker.Item label="Shirts" value="shirts" />*/}
+              {/*/!*</Picker>*!/*/}
+              <Text>category</Text>
+            </View>
+            <View style={{ flex: 1 }}>
+              {/*<Picker*/}
+              {/*selectedValue={this.state.selectedFilter}*/}
+              {/*onValueChange={itemValue =>*/}
+              {/*this.setState({ selectedFilter: itemValue })*/}
+              {/*}*/}
+              {/*style={{ flex: 1 }}*/}
+              {/*>*/}
+              {/*<Picker.Item label="filter1" value="filter1" />*/}
+              {/*<Picker.Item label="filter2" value="filter2" />*/}
+              {/*</Picker>*/}
+              <Text>picker1</Text>
+              {/*<Picker*/}
+              {/*selectedValue={this.state.selectedSortBy}*/}
+              {/*onValueChange={itemValue =>*/}
+              {/*this.setState({ selectedSortBy: itemValue })*/}
+              {/*}*/}
+              {/*style={{ flex: 1 }}*/}
+              {/*>*/}
+              {/*<Picker.Item label="sortby1" value="sortby1" />*/}
+              {/*<Picker.Item label="sortby2" value="sortby2" />*/}
+              {/*</Picker>*/}
+              <Text>picker2</Text>
+            </View>
           </View>
+        </View>
         <View style={{ flex: 5 }}>
           {this.state.inputText ? (
             <View>
--- index.js
+++ index.js
@@ -3,5 +3,11 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
-
+import firebase from 'react-native-firebase';
+firebase
+  .auth()
+  .signInAnonymously()
+  .then(user => {
+    console.warn(user.isAnonymous);
+  });
 AppRegistry.registerComponent(appName, () => App);
