03623318dacf3f57da4c12cd14a1efc8a1a8207e
Graham Matuszewski <matuszeg@gmail.com>
2018-11-26 19:52:46 -0800

    delete everything

--- app/themes/Colors.js
+++
@@ -1,7 +0,0 @@
-const colors = {
-    theme: 'rgb(191,174,159)',
-    inactiveTheme: 'rgb(206,189,173)',
-    tabBarTint: 'rgb(58,56,56)'
-}
-
-export default colors
--- src/utils/Fonts.js
+++
@@ -1,3 +0,0 @@
-export const Fonts = {
-    Prociono: 'Prociono-Regular'
-}
